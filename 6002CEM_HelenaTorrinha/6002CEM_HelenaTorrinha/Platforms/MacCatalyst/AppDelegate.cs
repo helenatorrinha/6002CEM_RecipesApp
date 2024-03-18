deb6799073376f5b2077f9dcb5cf344fe1d6cb06
Helena Torrinha <serranotoh@coventry.ac.uk>
2024-03-18 13:59:39 +0000

    Initial commit

---
+++ 6002CEM_HelenaTorrinha/6002CEM_HelenaTorrinha/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _6002CEM_HelenaTorrinha;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
